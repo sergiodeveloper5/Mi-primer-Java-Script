5795a2e0fd23f0ac1903bea4a9d4e4d9396b4d3c
sergiodeveloper5 <156307738+sergiodeveloper5@users.noreply.github.com>
2024-02-01 12:15:53 +0100

    Ejercicio2

---
+++ MiprimerJS/ejercicios JavaScript/03-Objetos/objeto.js
@@ -0,0 +1,15 @@
+let animal = {
+    nombre: "León",
+    tipo: "Mamífero",
+    habitat: "Sabana",
+    edad: 5,
+    color: "Dorado"
+};
+
+console.log(animal);
+
+// Cambiar edad y color
+animal.edad = 7; // Nueva edad
+animal.color = "Marrón claro"; // Nuevo color
+
+console.log(`Edad: ${animal.edad}, Color: ${animal.color}`);
